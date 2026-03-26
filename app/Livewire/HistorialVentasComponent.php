050fa1194b5fcd0d968c837f1282def9acf258dd
MarianoSS28 <salazar.mariano64@gmail.com>
2026-03-26 08:35:24 -0500

    first commit

---
+++ app/Livewire/HistorialVentasComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class HistorialVentasComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.historial-ventas-component');
+    }
+}
